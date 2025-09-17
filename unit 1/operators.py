c5366425b9963299a9faf5b1e48ddb1111bd4f6c
jpatton158 <jpatton158@boyslatin.net>
2025-09-17 14:18:23 +0000

    lebron part 2

--- unit 1/operators.py
+++ unit 1/operators.py
@@ -3,4 +3,12 @@ prin
 
 
 
-name= input('what is your name')
+#math
+# create a function that will calculate 2 numbers 
+# with different arithmetic operators 
+# Def calculate arithmetic operators 
+def calulate():
+    numx= input('please enter a number') 
+    numy = input('please enter another number') 
+     print(numx, numy)
+    Calculate()
---
+++ unit 1/unit 2/functiondata.py
@@ -0,0 +1,42 @@
+# How to create a functionn that passes data
+
+# step 1> create a fuinction defintion
+def bnbrefund(username, RefundAmount):
+    print('sorry'+ username + 'for youur canellation')     
+    print(' we will refund $' + RefundAmount+'amount to yourbank. ') 
+# step2. call the function/ excute instruction 
+# bnbrefund('jacob', '3000') 
+
+def bday(name,bday):
+    print ("my name is " + name + " and my bday is " + bday)
+
+# bday('jahleil', 'march 1') 
+
+def moneyconverter(dollar):
+    pennies= dollar * 100
+    print('my ' + str(dollar) + 'dollar(s) is equal to ' + str(pennies) + '   pennies.')
+   
+# moneyconverter(50) 
+
+def areaoftriangle(length, width):
+    triangle = length * width * 0.5
+    print('area')
+    print(triangle)
+
+#areaoftriangle(10,30)
+
+def calculatethearea(length, width, height):
+    area = 20*15*23 
+    print('Calculate')
+    print(area) 
+
+calculatethearea(20,10,23)
+   
+
+
+def fahrenheitconverter(far):
+    cel= far - 32 * 5/9
+    print('my cel is : ' + str(far))
+
+fahrenheitconverter(32)
+
