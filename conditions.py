e85fa63b7d14c64aa7e09681993873f929fc9150
jpatton158 <jpatton158@boyslatin.net>
2025-09-25 14:26:51 +0000

    conditions

---
+++ conditions.py
@@ -0,0 +1,33 @@
+#down = input('what down is it? ') 
+#yards = input('how man yards do you need to get another first down?')
+
+#if down == 1 and yards <= 5: 
+ #   print("run it")
+#elif down == 2 and yards <= 5: 
+ #   print("run it")
+#elif down == 3 and yards <=5:
+     #print("play action")
+
+def testtakerpermit(age): 
+     if age >= 16:
+        print("you may take your permit test")
+     else:
+        print("too young")
+
+
+testtakerpermit(14)
+    
+
+def postiveornegative(number):
+    if number >= 1:
+        print("this is postive")
+    else:  
+        print("this is negative")
+
+
+postiveornegative(15)
+
+
+def scorechecker(grade):
+    if grade > 80
+
